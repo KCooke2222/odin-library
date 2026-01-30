3814e5c2968dd627971cbb0ae5db022852f7de48
Kevin <>
2026-01-30 10:28:28 -0600

    Finish project

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const myLibrary = [
-  { title: "1984", author: "George Orwell", pages: 328, read: true },
-  { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, read: false },
-  { title: "Dune", author: "Frank Herbert", pages: 412, read: true },
+let myLibrary = [
+  new Book("1984", "George Orwell", 328, true),
+  new Book("The Hobbit", "J.R.R. Tolkien", 310, false),
+  new Book("Dune", "Frank Herbert", 412, true),
 ];
 
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -19,10 +20,22 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
 };
 
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
 function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+}
+
+function removeBookById(id) {
+  myLibrary = myLibrary.filter((book) => book.id !== id);
+}
+
+function getBookById(id) {
+  return myLibrary.find((book) => book.id === id);
 }
 
 function displayBooks() {
@@ -30,10 +43,40 @@ function displayBooks() {
   for (const book of myLibrary) {
     const bookDisplay = document.createElement("div");
     bookDisplay.className = "book";
-    ["title", "author", "pages", "read"].forEach((key) => {
+    bookDisplay.dataset.id = book.id;
+
+    // info
+    ["title", "author", "pages"].forEach((key) => {
       const p = document.createElement("p");
       p.textContent = book[key];
       bookDisplay.appendChild(p);
+    });
+
+    // read
+    const p = document.createElement("p");
+    p.classList.add(book.read ? "read" : "not-read");
+    p.textContent = book["read"] ? "Read" : "Not read";
+    bookDisplay.appendChild(p);
+    p.addEventListener("click", (e) => {
+      const bookDiv = e.target.closest(".book");
+      if (!bookDiv) return;
+
+      let book = getBookById(bookDiv.dataset.id);
+      book.toggleRead();
+
+      displayBooks();
+    });
+
+    // del button
+    const delBook = document.createElement("button");
+    delBook.textContent = "Delete";
+    bookDisplay.appendChild(delBook);
+    delBook.addEventListener("click", (e) => {
+      const bookDiv = e.target.closest(".book");
+      if (!bookDiv) return;
+
+      removeBookById(bookDiv.dataset.id);
+      displayBooks();
     });
 
     bookshelf.appendChild(bookDisplay);
@@ -65,3 +108,5 @@ form.addEventListener("submit", (e) => {
   form.reset();
   dialog.close();
 });
+
+displayBooks();
